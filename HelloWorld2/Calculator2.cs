6e93ae0cf2178cc9e49daf74593774aa66356778
Ahi188 <188aheli@gmail.com>
2021-11-29 14:23:33 +0530

    Calculator2

---
+++ HelloWorld2/Calculator2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld2
+{
+    public class Calculator2
+    {
+        public int x { get; set; }
+
+        public int y { get; set; }
+
+        public int Add()
+        {
+            return x + y;
+        }
+
+    }
+}
---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloWorld2;
+
+Console.WriteLine("Hello, World!");
+Calculator2 calc = new Calculator2();
+calc.x = 10;
+calc.y = 20;
+int result = calc.Add();
+Console.WriteLine(result);
