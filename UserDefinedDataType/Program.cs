4dd2e3c628a2cece889c96908d4cca6450b07793
Ahi188 <188aheli@gmail.com>
2021-12-01 12:45:39 +0530

    Student Array

--- UserDefinedDataType/Program.cs
+++ UserDefinedDataType/Program.cs
@@ -1,26 +1,66 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using UserDefinedDataType;
-for (int i = 0; i < 5; i++)
+
+    Student[] studentarray = null;
+    int size = 0;
+
+for (; ; )
 {
-    Student student = new Student();
+    Console.WriteLine("1. Enter Student Details.");
+    Console.WriteLine("2. Display Student Details.");
+    String input = Console.ReadLine();
+    if (input == "1")
+    {
+        Console.WriteLine("Enter the number of students:");
 
-    Console.WriteLine("Please enter information about the student");
-    Console.WriteLine("Enter First Name");
-    student.FirstName = Console.ReadLine();
+        int i = Convert.ToInt32(Console.ReadLine());
+        size = i;
+        studentarray = new Student[] { };
 
-    Console.WriteLine("Enter LastName");
-    student.LastName = Console.ReadLine();
+        if (i != 0)
+        {
+            for (int j = 0; j < i; j++)
+            {
 
-    Console.WriteLine("Enter Roll Number");
-    student.Roll = Convert.ToInt32(Console.ReadLine());
+                Student student = new Student();
+                Console.WriteLine($"Enter details for student{j + 1}");
 
-    Console.WriteLine("Enter Marks Secured");
-    student.MarksSecured = Convert.ToDouble(Console.ReadLine());
-    
-    Console.WriteLine("Thank You for sharing your Information. Your Details are as follows:");
-    //printing
-    Console.WriteLine($"FirstName: {student.FirstName}");
-    Console.WriteLine($"LastName: {student.LastName}");
-    Console.WriteLine($"Roll Number: {student.Roll}");
-    Console.WriteLine($"Marks Secured: {student.MarksSecured}");
-}
+                Console.WriteLine("Enter First Name");
+                student.FirstName = Console.ReadLine();
+
+                Console.WriteLine("Enter LastName");
+                student.LastName = Console.ReadLine();
+
+                Console.WriteLine("Enter Roll Number");
+                student.Roll = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Marks Secured");
+                student.MarksSecured = Convert.ToDouble(Console.ReadLine());
+
+                studentarray = studentarray.Append(student).ToArray();
+            }
+        }
+
+    }
+
+    else if (input == "2")
+    {
+
+        Console.WriteLine("Thank You for sharing your Information. Your Details are as follows:");
+        for (int i = 0; i < size; i++)
+        {
+            Student student = studentarray[i];
+            Console.WriteLine($"FirstName: {student.FirstName}");
+            Console.WriteLine($"LastName: {student.LastName}");
+            Console.WriteLine($"Roll Number: {student.Roll}");
+            Console.WriteLine($"Marks Secured: {student.MarksSecured}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid Input");
+        break;
+    }
+    }
+
+
