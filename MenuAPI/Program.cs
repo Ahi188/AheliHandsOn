32d8dcb48e7bf218b93f3b7428507b3f68001843
Ahi188 <188aheli@gmail.com>
2022-03-07 18:14:33 +0530

    Spice Den with MenuAPI

--- MenuAPI/Program.cs
+++ MenuAPI/Program.cs
@@ -5,6 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("https://locahost:7083")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
 builder.Services.AddDbContext<TrainingDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CSName")));
 builder.Services.AddScoped<IMenuDAL, MenuDAL>();
 
@@ -18,6 +28,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors(builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
     app.UseSwagger();
     app.UseSwaggerUI();
 }
--- SpiceDenwithAPI/wwwroot/js/site.js
+++ SpiceDenwithAPI/wwwroot/js/site.js
@@ -2,48 +2,129 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-        //for hover table
-    function openplace(evt, mallName) {
-            var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-            } //hidden tab contents
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+$(document).ready(function () {
+    $.ajax({
+        url: 'https://localhost:7083/api/Menu/GetMenu',
+        type: 'GET',
+        success: function (data1) {
+            var html = '';
+            for (var index = 0; index < data1.length; index++) {
+                console.log(data1[index]);
+                html += ('<option value="' + data1[index].menuID + '">' + data1[index].cusine + '</option>');
             }
-    document.getElementById(mallName).style.display = "block";
-    evt.currentTarget.className += " active";
-        } //checking to see if it is matching with the button hovederd upon.
+            $('#menuList').append(html);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("Status: " + textStatus);
+            alert("Error: " + errorThrown);
+        }
+    });
+   
+    $('#menuList').change(function () {
+        var menuselected = $('#menuList').val();
+        //alert(menuselected);
+        $.ajax({
+            url: 'https://localhost:7083/api/Menu/GetChoice',
+            type: 'GET',
+            success: function (data1) {
+                var html = '<option value=" ">' + "Select" + '</option>';
+                for (var index = 0; index < data1.length; index++) {
+                    console.log(data1[index]);
+                    html += ('<option value="' + data1[index].choiceID + '">' + data1[index].category + '</option>');
+                }
+                $('#choiceList').html(html);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert("Status: " + textStatus);
+                alert("Error: " + errorThrown);
+            }
+        });
+
+    });
+    $('#choiceList').change(function () {
+        var choiceList = $('#choiceList').val();
+        //alert(choiceList);
+        var menuselected = $('#menuList').val();
+        $.ajax({
+            url: 'https://localhost:7083/api/Menu/GetMenuCard?Mid=' + menuselected+'&Cid=' + choiceList,
+
+            type: 'GET',
+            success: function (data1) {
+                var html = '';
+                for (var index = 0; index < data1.length; index++) {
+                    console.log(data1[index]);
+                    html += ('<tr>'+
+                        '<td>'+ data1[index].dishId+'</td>'+
+                        '<td>' +data1[index].dishName+'</td>'+
+                        '<td>'+ data1[index].price+'</td>'+
+                    '</tr>');
+                }
+                $('#tbody').html(html);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert("Status: " + textStatus);
+                alert("Error: " + errorThrown);
+            }
+        });
+    });
+    $.ajax({
+        url: 'https://localhost:7083/api/Menu/AddDish',
+        type: 'POST',
+        success: function (response) {
+
+        }
+    });
+   
+});
+
+
+//change function 
+
+//onchange event, include a form, radio buttons or drop down, make ajax call accordingly.
+
+
+ //for hover table
+    //function openplace(evt, mallName) {
+    //        var i, tabcontent, tablinks;
+    //tabcontent = document.getElementsByClassName("tabcontent");
+    //for (i = 0; i < tabcontent.length; i++) {
+    //    tabcontent[i].style.display = "none";
+    //        } //hidden tab contents
+    //tablinks = document.getElementsByClassName("tablinks");
+    //for (i = 0; i < tablinks.length; i++) {
+    //    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    //        }
+    //document.getElementById(mallName).style.display = "block";
+    //evt.currentTarget.className += " active";
+    //    } //checking to see if it is matching with the button hovederd upon.
 
     //for slide show
-    var slideIndex = 1;
-    showSlides(slideIndex);
+    //var slideIndex = 1;
+    //showSlides(slideIndex);
 
-    // Next/previous controls
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-        }
+    //// Next/previous controls
+    //function plusSlides(n) {
+    //    showSlides(slideIndex += n);
+    //    }
 
-    // Thumbnail image controls
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-        }
+    //// Thumbnail image controls
+    //function currentSlide(n) {
+    //    showSlides(slideIndex = n);
+    //    }
 
-    function showSlides(n) {
-            var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-            if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-            }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-            }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-        }
+    //function showSlides(n) {
+    //        var i;
+    //var slides = document.getElementsByClassName("mySlides");
+    //var dots = document.getElementsByClassName("dot");
+    //        if (n > slides.length) {slideIndex = 1}
+    //if (n < 1) {slideIndex = slides.length}
+    //for (i = 0; i < slides.length; i++) {
+    //    slides[i].style.display = "none";
+    //        }
+    //for (i = 0; i < dots.length; i++) {
+    //    dots[i].className = dots[i].className.replace(" active", "");
+    //        }
+    //slides[slideIndex - 1].style.display = "block";
+    //dots[slideIndex - 1].className += " active";
+    //    }
+//ajax get call the api and get the data
